56324727c5d5623ff5ee3e6de17f45e219e97af3
Danielccc3 <82000069+Danielccc3@users.noreply.github.com>
2023-11-27 14:57:52 -0300

    Update Base.cs

--- MeuFocoTotalApi/Common/Base.cs
+++ MeuFocoTotalApi/Common/Base.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace HexagonProGraneisRecepcaoApi.Common
+namespace MeuFocoTotalApi.Common
 {
     public class Base
     {
